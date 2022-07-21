05860b1c2e62885b2713a152bf84b8ba0d309919
Morgan Scholz <mscholz.dev@gmail.com>
2022-07-21 16:19:57 +0200

    udpate

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -24,7 +24,6 @@ export default class ProfileApi {
           headers: {
             accept: "application/json",
           },
-          url: process.env.BASE_URL_API,
         }
       )
       .then((res) => res.data);
