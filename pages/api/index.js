aba72f023f3401302822ebbb8b38d443d65a9254
mscholz.dev <mscholz.dev@gmail.com>
2022-07-31 19:29:01 +0200

    update

--- pages/api/index.js
+++ pages/api/index.js
@@ -6,4 +6,8 @@ export default axios.create({
   baseURL: `${process.env.BASE_URL_API}/api`,
   // access to cookies
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
 });
