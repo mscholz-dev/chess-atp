a85b54ac0d5508f8703e123a1dbefe9feb202972
mscholz.dev <mscholz.dev@gmail.com>
2024-05-13 15:46:44 +0200

    add log

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,28 @@
+import React, { useEffect } from "react";
 import "../styles/main.scss";
 import "regenerator-runtime/runtime";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(async () => {
+    const logoStyle = (logo) => `
+      background-image: url(${`data:image/svg+xml;base64,${btoa(logo)}`});
+      background-repeat: no-repeat;
+      background-size: 32px;
+      color: white;
+      font-family: sans-serif;
+      font-size: 32px;
+      padding-left: 42px;
+    `;
+
+    const logo1 = await (await fetch(`/mscholzdev/logo-1.svg`)).text();
+    console.info("%cCréé par Morgan Scholz avec ❤️", logoStyle(logo1));
+
+    const logo2 = await (await fetch(`/mscholzdev/logo-2.svg`)).text();
+    console.info("%chttps://mscholz.dev", logoStyle(logo2));
+
+    const logo3 = await (await fetch(`/mscholzdev/logo-3.svg`)).text();
+    console.info("%cmscholz.dev@gmail.com", logoStyle(logo3));
+  }, []);
+
   return <Component {...pageProps} />;
 }
--- templates/layouts/Page.js
+++ templates/layouts/Page.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import Header from "./Header";
 import Toast from "../components/Toast";
 
-export default function Page({ title, children, toast, handleToastClick, auth, locale, language }) {
+export default function Page({
+  title,
+  children,
+  toast,
+  handleToastClick,
+  auth,
+  locale,
+  language,
+}) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {toast && <Toast display={toast.display} event={toast.event} title={toast.title} text={toast.text} handleClick={handleToastClick} />}
+      {toast && (
+        <Toast
+          display={toast.display}
+          event={toast.event}
+          title={toast.title}
+          text={toast.text}
+          handleClick={handleToastClick}
+        />
+      )}
       <Header auth={auth} locale={locale} language={language} />
       <main className="main-wrapper">{children}</main>
     </>
