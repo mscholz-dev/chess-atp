254cd4ec69910c02095fdce85e187b64129b75d7
mscholz.dev <mscholz.dev@gmail.com>
2024-05-13 15:51:43 +0200

    update log

--- pages/_app.js
+++ pages/_app.js
@@ -3,25 +3,28 @@ import "../styles/main.scss";
 import "regenerator-runtime/runtime";
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(async () => {
-    const logoStyle = (logo) => `
-      background-image: url(${`data:image/svg+xml;base64,${btoa(logo)}`});
-      background-repeat: no-repeat;
-      background-size: 32px;
-      color: white;
-      font-family: sans-serif;
-      font-size: 32px;
-      padding-left: 42px;
-    `;
+  useEffect(() => {
+    const logoLog = async () => {
+      const logoStyle = (logo) => `
+        background-image: url(${`data:image/svg+xml;base64,${btoa(logo)}`});
+        background-repeat: no-repeat;
+        background-size: 32px;
+        color: white;
+        font-family: sans-serif;
+        font-size: 32px;
+        padding-left: 42px;
+      `;
 
-    const logo1 = await (await fetch(`/mscholzdev/logo-1.svg`)).text();
-    console.info("%cCréé par Morgan Scholz avec ❤️", logoStyle(logo1));
+      const logo1 = await (await fetch(`/mscholzdev/logo-1.svg`)).text();
+      console.info("%cCréé par Morgan Scholz avec ❤️", logoStyle(logo1));
 
-    const logo2 = await (await fetch(`/mscholzdev/logo-2.svg`)).text();
-    console.info("%chttps://mscholz.dev", logoStyle(logo2));
+      const logo2 = await (await fetch(`/mscholzdev/logo-2.svg`)).text();
+      console.info("%chttps://mscholz.dev", logoStyle(logo2));
 
-    const logo3 = await (await fetch(`/mscholzdev/logo-3.svg`)).text();
-    console.info("%cmscholz.dev@gmail.com", logoStyle(logo3));
+      const logo3 = await (await fetch(`/mscholzdev/logo-3.svg`)).text();
+      console.info("%cmscholz.dev@gmail.com", logoStyle(logo3));
+    };
+    logoLog();
   }, []);
 
   return <Component {...pageProps} />;
